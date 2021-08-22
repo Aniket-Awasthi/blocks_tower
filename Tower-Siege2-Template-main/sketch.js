3d053ad78fb5d423aaf42c673c92c3c635249963
Aniket-Awasthi <79520266+Aniket-Awasthi@users.noreply.github.com>
2021-08-22 10:57:22 +0530

    Add files via upload

---
+++ Tower-Siege2-Template-main/sketch.js
@@ -0,0 +1,213 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var ball,ground;
+var stand1,stand2;
+var slingShot;
+var rock_img;
+
+function preload(){
+  rock_img=loadImage("rock.png");
+}
+function setup() {
+
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
+ 
+  //level one
+  block1 = new Block(300,275,30,40);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  //level three
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  //top
+  block16 = new Block(390,155,30,40);
+
+  //set 2 for second stand
+  //level one
+  blocks1 = new Block(640,175,30,40);
+  blocks2 = new Block(670,175,30,40);
+  blocks3 = new Block(700,175,30,40);
+  blocks4 = new Block(730,175,30,40);
+  blocks5 = new Block(760,175,30,40);
+  //level two
+  blocks6 = new Block(670,135,30,40);
+  blocks7 = new Block(700,135,30,40);
+  blocks8 = new Block(730,135,30,40);
+  //top
+  blocks9 = new Block(700,95,30,40);
+
+ 
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:120,y:200});
+
+}
+
+function draw() {
+  background(56,44,44); 
+ 
+  imageMode(CENTER);
+  image(rock_img ,ball.position.x,ball.position.y,40,40);
+
+
+  stroke(0,0,0);
+  fill("white");
+  textSize(18);
+  fill("lightyellow");
+  text("Drag the Hexagonal Stone and Release it, to take short Press SPACE for 2nd chance and ENTER to reset blogs",0,30);
+
+  ground.display();
+  stand1.display();
+  stand2.display();
+
+  strokeWeight(2);
+  stroke(0,0,0);
+  
+  fill("skyblue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("turquoise");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("grey");
+  block16.display();
+  fill("skyblue");
+  blocks1.display();
+  blocks2.display();
+  blocks3.display();
+  blocks4.display();
+  blocks5.display();
+  fill("turquoise");
+  blocks6.display();
+  blocks7.display();
+  blocks8.display();
+  fill("pink")
+  blocks9.display();
+  fill("gold");
+
+
+  
+  slingShot.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
+}
+
+function keyPressed(){
+  //if(keyCode === 20)
+  //if(keyCode === 22)
+  //if(keyCode === 30)
+  if(keyCode === 32)
+  {
+      slingShot.attach(this.ball);
+      Matter.Body.setPosition(this.ball,{x:50,y:200});
+  }
+  if(keyCode === 13){
+
+    block1.distory();
+    block2.distory();
+    block3.distory();
+    block4.distory();
+    block5.distory();
+    block6.distory();
+    block7.distory();
+
+    block8.distory();
+    block9.distory();
+    block10.distory();
+    block11.distory();
+    block12.distory();
+
+    block13.distory();
+    block14.distory();
+    block15.distory();
+
+    block16.distory();
+
+    blocks1.distory();
+    blocks2.distory();
+    blocks3.distory();
+    blocks4.distory();
+    blocks5.distory();
+
+    blocks6.distory();
+    blocks7.distory();
+    blocks8.distory();
+
+    blocks9.distory();
+   
+ block1 = new Block(300,275,30,40);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  //level three
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  //top
+  block16 = new Block(390,155,30,40);
+
+  //set 2 for second stand
+  //level one
+  blocks1 = new Block(640,175,30,40);
+  blocks2 = new Block(670,175,30,40);
+  blocks3 = new Block(700,175,30,40);
+  blocks4 = new Block(730,175,30,40);
+  blocks5 = new Block(760,175,30,40);
+  //level two
+  blocks6 = new Block(670,135,30,40);
+  blocks7 = new Block(700,135,30,40);
+  blocks8 = new Block(730,135,30,40);
+  //top
+  blocks9 = new Block(700,95,30,40);
+
+ 
+  }
+}
